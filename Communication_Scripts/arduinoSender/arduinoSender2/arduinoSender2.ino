47c5d276f2931567ce908ec025576c8c56835f07
Taren P <63258880+TarenP@users.noreply.github.com>
2022-02-12 17:09:43 -0800

    Update arduinoSender2.ino

--- Communication_Scripts/arduinoSender/arduinoSender2/arduinoSender2.ino
+++ Communication_Scripts/arduinoSender/arduinoSender2/arduinoSender2.ino
@@ -1,11 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600)
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(",");
-  Serial.print(analogRead(A0)); Serial.print(",");
+  Serial.print(analogRead(A0));
   delay(100);
 }
