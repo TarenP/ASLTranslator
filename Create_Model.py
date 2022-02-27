70f498fc94bd59a2a0e5a1e5eaefc2ca0a3dc82a
Taren P <63258880+TarenP@users.noreply.github.com>
2022-02-27 11:35:50 -0800

    Update Create_Model.py

--- Create_Model.py
+++ Create_Model.py
@@ -37,89 +37,57 @@ def main():
         mat = [ [ 3, 2, 4], [5, 6, 1], [8, 1, 3] ]
         
         print( getResult(mat) )
-    #     #print(Array2d_result)
-    #     length = len(Array2d_result)
-    #    # print(length)
-    #     l = length/dpts #how many datapoints to extract from the csv file
-    #     l = math.trunc(l)
-    #     #print(l)
-    #     #print(dpts)
-    #     for i in range(len(Array2d_result)):
-    #         if i % l != 0:
-    #             try:
-    #                 Array2d_result = np.delete(Array2d_result, i)
-    #             except:
-    #                 pass
-    #     if len(Array2d_result) > 10:
-    #         for i in range(len(Array2d_result)):
-    #             if i > 10:
-    #                 Array2d_result = np.delete(Array2d_result, i)   
-    #     data.append(Array2d_result)
-    # print(len(data))
-    # print(data)
+        #print(Array2d_result)
+        length = len(Array2d_result)
+       # print(length)
+        l = length/dpts #how many datapoints to extract from the csv file
+        l = math.trunc(l)
+        #print(l)
+        #print(dpts)
+        for i in range(len(Array2d_result)):
+            if i % l != 0:
+                try:
+                    Array2d_result = np.delete(Array2d_result, i)
+                except:
+                    pass
+        if len(Array2d_result) > 10:
+            for i in range(len(Array2d_result)):
+                if i > 10:
+                    Array2d_result = np.delete(Array2d_result, i)   
+        data.append(Array2d_result)
+    print(len(data))
+    print(data)
 
 
     #split data into 20% test and 80% train
-    # x_train, x_test, y_train, y_test = train_test_split(data, target, test_size= 0.2)
-    # # print(len(x_train))
-    # # print(len(x_test))
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size= 0.2)
+    # print(len(x_train))
+    # print(len(x_test))
     
-    # model = LogisticRegression()
-    # #train model
-    # model.fit(x_train, y_train)
-    # #print the accuracy of model against the test data
-    # print(model.score(x_test, y_test))
+    model = LogisticRegression()
+    #train model
+    model.fit(x_train, y_train)
+    #print the accuracy of model against the test data
+    print(model.score(x_test, y_test))
     
-    # with open('model_pickle', 'wb') as f:
-    #     pickle.dump(model, f)
+    with open('model_pickle', 'wb') as f:
+        pickle.dump(model, f)
 
-def compress(arr):
- 
-    # Stores the required integer
-    ans = 0
-    getBit = 1
-    
-    # Checking for each position
-    for i in range(32):
-    
-        S = 0
-        NS = 0
-    
-        for j in arr:
-    
-            # Count set and unset bits
-            if getBit&j:
-                S += 1
-            else:
-                NS += 1
-        
-        # If count of set bits exceeds
-        # count of unset bits
-        if S > NS:
-    
-        # Add value of set bits to ans
-            ans += 2**i
-        getBit <<= 1
-    
-    return ans
-    
+
     # Function to compress
     # matrix to a single number
 def getResult(mat):
     
     # Stores compressed array
     compressedArr = []
-    
+    print(len(mat))
     for i in range(len(mat)):
         col = []
         for j in range(len(mat[0])):
             col.append(mat[j][i])
+
     
-        # Compress all columns
-        # to a single number 
-        compressedArr.append(compress(col))
-    
-    return compress(compressedArr)
+    return compressedArr
  
 
 if __name__ == '__main__':
