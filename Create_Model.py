342f54147bf20bdd9c9db41af062c3918b3f9be0
Taren P <63258880+TarenP@users.noreply.github.com>
2022-02-26 17:06:49 -0800

    Update Create_Model.py

--- Create_Model.py
+++ Create_Model.py
@@ -5,7 +5,6 @@ This script is used for creating a new model from the data already in the databa
 from time import sleep
 import random
 import csv
-import numpy as np
 from numpy import mod
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -25,29 +24,28 @@ def main():
         with open(filename, 'r') as f:
             f = open(filename, encoding='UTF8')
             csv_reader = csv.reader(f)
-            x = list(csv_reader)
-            result = np.array(x).astype("string")
-            result = np.array(list(csv.reader(open("test.csv", "rb"), delimiter=","))).astype("string")
-            print(result)
-            # data.append(gesture)
-            # #remove the filepath from the name
-            # name = filename.replace("./Gesture_Database\\", "")
-            # name = name.replace(".csv", "")
-            # target.append(name)
+            for line in csv_reader:
+                #add the data from each line from the gesture's file to the array that will be used to train the model
+                gesture = [int(line[0]), int(line[1]), int(line[2]), int(line[3]), int(line[4]),int(line[5]), int(line[6]), int(line[7]), int(line[8]), int(line[9]), int(line[10])]
+                data.append(gesture)
+                #remove the filepath from the name
+                name = filename.replace("./Gesture_Database\\", "")
+                name = name.replace(".csv", "")
+                target.append(name)
 
-    # #split data into 20% test and 80% train
-    # x_train, x_test, y_train, y_test = train_test_split(data, target, test_size= 0.2)
-    # # print(len(x_train))
-    # # print(len(x_test))
+    #split data into 20% test and 80% train
+    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size= 0.2)
+    # print(len(x_train))
+    # print(len(x_test))
     
-    # model = LogisticRegression()
-    # #train model
-    # model.fit(x_train, y_train)
-    # #print the accuracy of model against the test data
-    # print(model.score(x_test, y_test))
+    model = LogisticRegression()
+    #train model
+    model.fit(x_train, y_train)
+    #print the accuracy of model against the test data
+    print(model.score(x_test, y_test))
     
-    # with open('model_pickle', 'wb') as f:
-    #     pickle.dump(model, f)
+    with open('model_pickle', 'wb') as f:
+        pickle.dump(model, f)
 
 if __name__ == '__main__':
     main()
