d537495bdf14c72d2cf79f28d9103eaba3460359
Taren P <63258880+TarenP@users.noreply.github.com>
2022-02-26 19:05:21 -0800

    Update Create_Model.py

--- Create_Model.py
+++ Create_Model.py
@@ -15,7 +15,8 @@ import pickle
 
 #generate an artificial dataset
 gesture = []
-data = np.array([])
+csvData = []
+data = []
 target =[]
 
 def main():
@@ -27,13 +28,15 @@ def main():
             csvArray=[]
             for line in csv_reader:
                 #add the data from each line from the gesture's file to the array that will be used to train the model
-                gesture = np.array([int(float(line[0])), int(float(line[1])), int(float(line[2])), int(float(line[3])), int(float(line[4])),int(float(line[5])), int(float(line[6])), int(float(line[7])), int(float(line[8])), int(float(line[9]))])
+                gesture = [int(float(line[0])), int(float(line[1])), int(float(line[2])), int(float(line[3])), int(float(line[4])),int(float(line[5])), int(float(line[6])), int(float(line[7])), int(float(line[8])), int(float(line[9]))]
+            csvData.append[gesture]
             #remove the filepath from the name
             name = filename.replace("./Gesture_Database/", "")
             name = name.replace(".csv", "")
             name = ''.join((x for x in name if not x.isdigit()))
             target.append(name)
-            print(gesture)
+        data.append[csvData]
+        print(data)
 
     #split data into 20% test and 80% train
     x_train, x_test, y_train, y_test = train_test_split(data, target, test_size= 0.2)
