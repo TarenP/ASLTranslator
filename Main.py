b641842bdba4b3af34cf9b9424aad3fc0087b54e
Taren P <63258880+TarenP@users.noreply.github.com>
2022-02-27 16:06:45 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,68 +31,69 @@ def main():
 #Return True when button is pressed
 def Button():
     global button
-    if button == 'L':
-        while True:
-            #get the difference between before and after move is performed for each sensor
-            print("Keep all fingers straight, and press button to start")
-            #Initial flex sensor values for each finger
-            line1=ser1.readline().decode()
-            line2=ser2.readline().decode()
-        #     print(line1)
-        #     print(line2)
+    while True:
+        if button == 'L':
+            while True:
+                #get the difference between before and after move is performed for each sensor
+                print("Keep all fingers straight, and press button to start")
+                #Initial flex sensor values for each finger
+                line1=ser1.readline().decode()
+                line2=ser2.readline().decode()
+            #     print(line1)
+            #     print(line2)
 
-            # Take out the commas. Parse the string into a list.
-            parsed1 = line1.split(',')
-            parsed2 = line2.split(',')
+                # Take out the commas. Parse the string into a list.
+                parsed1 = line1.split(',')
+                parsed2 = line2.split(',')
 
-            # rstrip() function removes trailing characters like
-            # the new line character '\n' and '/r'. Also removes
-            # white space.
-            parsed1 = [x.rstrip() for x in parsed1]
-            parsed2 = [x.rstrip() for x in parsed2]
+                # rstrip() function removes trailing characters like
+                # the new line character '\n' and '/r'. Also removes
+                # white space.
+                parsed1 = [x.rstrip() for x in parsed1]
+                parsed2 = [x.rstrip() for x in parsed2]
 
-            #print(parsed1)
-            #print(parsed2)
+                #print(parsed1)
+                #print(parsed2)
 
-            if(len(parsed1)> 9 or len(parsed2)>9):
-                # We add the '0' character to the end of each item in the 
-                # parsed list. This makes sure that there are no empty
-                # strings in the list. Adding 0 makes sure that we have
-                # at least 6 string values we can convert into integers.
-                # Dividing by 10 removes the trailing 0 but it makes the integer a float.
-                # We then have to convert the float to an integer.
-                
-                #depending on which arduino gets assigned parsed1
-                try:
-                    finger0 = parsed1[0]
-                    finger1 = parsed1[1]
-                    finger2 = parsed1[2]
-                    finger3 = parsed1[3]
-                    finger4 = parsed2[0]
-                    ax = parsed1[5]
-                    ay = parsed1[6]
-                    az = parsed1[7]
-                    gx = parsed1[8]
-                    gy = parsed1[9]
-                    gz = parsed1[10]
-                    button = parsed2[1]
-                except:
-                    finger0 = parsed2[0]
-                    finger1 = parsed2[1]
-                    finger2 = parsed2[2]
-                    finger3 = parsed2[3]
-                    finger4 = parsed1[0]
-                    ax = parsed2[5]
-                    ay = parsed2[6]
-                    az = parsed2[7]
-                    gx = parsed2[8]
-                    gy = parsed2[9]
-                    gz = parsed2[10]
-                    button = parsed1[1]
-                    print(button)
-                if(button == 'H'): # If button has been pressed
-                    print("pressed")
-                    return
+                if(len(parsed1)> 9 or len(parsed2)>9):
+                    # We add the '0' character to the end of each item in the 
+                    # parsed list. This makes sure that there are no empty
+                    # strings in the list. Adding 0 makes sure that we have
+                    # at least 6 string values we can convert into integers.
+                    # Dividing by 10 removes the trailing 0 but it makes the integer a float.
+                    # We then have to convert the float to an integer.
+                    
+                    #depending on which arduino gets assigned parsed1
+                    try:
+                        finger0 = parsed1[0]
+                        finger1 = parsed1[1]
+                        finger2 = parsed1[2]
+                        finger3 = parsed1[3]
+                        finger4 = parsed2[0]
+                        ax = parsed1[5]
+                        ay = parsed1[6]
+                        az = parsed1[7]
+                        gx = parsed1[8]
+                        gy = parsed1[9]
+                        gz = parsed1[10]
+                        button = parsed2[1]
+                    except:
+                        finger0 = parsed2[0]
+                        finger1 = parsed2[1]
+                        finger2 = parsed2[2]
+                        finger3 = parsed2[3]
+                        finger4 = parsed1[0]
+                        ax = parsed2[5]
+                        ay = parsed2[6]
+                        az = parsed2[7]
+                        gx = parsed2[8]
+                        gy = parsed2[9]
+                        gz = parsed2[10]
+                        button = parsed1[1]
+                        print(button)
+                    if(button == 'H'): # If button has been pressed
+                        print("pressed")
+                        return
 
 #Record Gesture
 def Record():
