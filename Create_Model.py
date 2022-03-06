bb7e72a144a7af2cddb912e39e3330082445c41c
Taren P <63258880+TarenP@users.noreply.github.com>
2022-03-06 12:54:05 -0800

    Update Create_Model.py

--- Create_Model.py
+++ Create_Model.py
@@ -22,12 +22,10 @@ def main():
         name = name.replace(".csv", "")
         name = ''.join((x for x in name if not x.isdigit()))
         with open(filename, "r") as f:
-            CSVData = csv.reader(f, delimeter = "\t")
+            CSVData = csv.reader(f)
             for i, line in enumerate(CSVData):
-                print(line)
-        target.append(name)
-        Array2d_result = getResult(Array2d_result)
-        data.append(Array2d_result)
+                target.append(name)
+                data.append(line)
         #print(data)
 
 
@@ -45,53 +43,6 @@ def main():
     with open('model_pickle', 'wb') as f:
         pickle.dump(model, f)
 
-
-    # Function to compress
-    # matrix to a single number
-def getResult(mat):
-    
-    # Stores compressed array
-    compressedArr = []
-    moving = False #tell if the gesture has a time axis or not
-    for i in range(len(mat[0])):
-        col = []
-        for j in range(len(mat)):
-            col = np.append(col, mat[j][i])
-        #print(np.percentile(col,90) - np.percentile(col,10))
-        if i >= 5 and i <= 7: #accelerometer data
-            if np.percentile(col,90) - np.percentile(col,10) >= 4:
-                moving = True
-        if i >= 8: #gyro data
-            if np.percentile(col,90) - np.percentile(col,10) >= 200:
-                moving = True
-        if i <= 4: #Fingers data
-            if np.percentile(col,90) - np.percentile(col,10) >= 300:
-                moving = True
-            
-    for i in range(len(mat[0])):
-        col = []
-        for j in range(len(mat)):
-            col = np.append(col, mat[j][i])
-        if moving:
-            pass #don't filter data
-        else:
-            mean = np.mean(col)
-            std = np.std(col)
-            std_arr = np.bitwise_and(col <= (mean + std), col >= (mean - std))
-            col[std_arr]
-        
-        append = list_average(col)
-        compressedArr.append(append)
-
-    return compressedArr
- 
-def list_average(num):
-    sum_num = 0
-    for t in num:
-        sum_num = sum_num + t           
-
-    avg = sum_num / len(num)
-    return avg
 
 if __name__ == '__main__':
     main()
