fb5cd1d6d5f45cb079d2096659b4c44292dbf044
Taren P <63258880+TarenP@users.noreply.github.com>
2022-03-06 15:53:59 -0800

    Update Create_Model.py

--- Create_Model.py
+++ Create_Model.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import os,glob
 import pickle
-import csv
 
 
 #generate an artificial dataset
@@ -17,16 +16,17 @@ target =[]
 def main():
     #Go through every gesture's file in the database
     for filename in glob.glob(os.path.join('./Gesture_Database', '*.csv')):
+        CSVData = open(filename)
+        Array2d_result = np.loadtxt(CSVData, delimiter=",")
+        
         #remove the filepath from the name
         name = filename.replace("./Gesture_Database/", "")
         name = name.replace(".csv", "")
         name = ''.join((x for x in name if not x.isdigit()))
-        with open(filename, "r") as f:
-            CSVData = csv.reader(f)
-            for line in enumerate(CSVData):
-                line =[int(line) for i in line]
-                target.append(name)
-                data.append(line)
+        for i in Array2d_result:
+            target.append(name)
+            i = i.astype(int)
+            data.append(i)
         print(data)
 
 
@@ -44,6 +44,52 @@ def main():
     with open('model_pickle', 'wb') as f:
         pickle.dump(model, f)
 
+
+    # Function to compress
+    # matrix to a single number
+def getResult(mat):
+    
+    # Stores compressed array
+    compressedArr = []
+    moving = False #tell if the gesture has a time axis or not
+    for i in range(len(mat[0])):
+        col = []
+        for j in range(len(mat)):
+            col = np.append(col, mat[j][i])
+        #print(np.percentile(col,90) - np.percentile(col,10))
+        if i >= 5 and i <= 7: #accelerometer data
+            if np.percentile(col,90) - np.percentile(col,10) >= 4:
+                moving = True
+        if i >= 8: #gyro data
+            if np.percentile(col,90) - np.percentile(col,10) >= 200:
+                moving = True
+        if i <= 4: #Fingers data
+            if np.percentile(col,90) - np.percentile(col,10) >= 300:
+                moving = True
+            
+    for i in range(len(mat[0])):
+        col = []
+        for j in range(len(mat)):
+            col = np.append(col, mat[j][i])
+        if moving:
+            pass #don't filter data
+        else:
+            mean = np.mean(col)
+            std = np.std(col)
+            std_arr = np.bitwise_and(col <= (mean + std), col >= (mean - std))
+            col[std_arr]
+        append = list_average(col)
+        compressedArr.append(append)
+
+    return compressedArr
+ 
+def list_average(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t           
+
+    avg = sum_num / len(num)
+    return avg
 
 if __name__ == '__main__':
     main()
